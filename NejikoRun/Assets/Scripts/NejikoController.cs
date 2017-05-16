835a5e2954f841420c002da919a9e6b5badc68f2
Akihisa Sakai <akihi1020@gmail.com>
2017-05-16 20:16:27 +0900

    NejikoRun ステージ作成まで

---
+++ NejikoRun/Assets/Scripts/NejikoController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NejikoController : MonoBehaviour
+{
+
+	const int MinLane = -2;
+	const int MaxLane = 2;
+	const float LaneWidth = 1.0f;
+
+	CharacterController controller;
+	Animator animator;
+
+	Vector3 moveDirection = Vector3.zero;
+	int targetLane;
+
+	public float gravity;
+	public float speedZ;
+	public float speedX;
+	public float speedJump;
+	public float accelerationZ;
+
+	// Use this for initialization
+	void Start()
+	{
+		// 必要なコンポーネントを自動取得
+		controller = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		// デバッグ用
+		if (Input.GetKeyDown("left")) {
+			MoveToLeft();
+		}
+		if (Input.GetKeyDown("right")) {
+			MoveToRight();
+		}
+		if (Input.GetKeyDown("space")) {
+			Jump();
+		}
+
+		// 徐々に加速しZ方向に常に前身させる
+		float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+		moveDirection.z = Mathf.Clamp(acceleratedZ, 0, speedZ);
+
+		// X方向は目標のポジションまでの差分の割合で速度を計算
+		float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
+		moveDirection.x = ratioX * speedX;
+        
+		// 重力分の力を毎フレーム追加
+		moveDirection.y -= gravity * Time.deltaTime;
+
+		// 移動実行
+		Vector3 globalDirection = transform.TransformDirection(moveDirection);
+		controller.Move(globalDirection * Time.deltaTime);
+
+		// 移動後接地してたらY方向の速度はリセットする
+		if (controller.isGrounded) {
+			moveDirection.y = 0;
+		}
+
+		// 速度が0以上なら走っているフラグをtrueにする
+		animator.SetBool("run", moveDirection.z > 0.0f);
+	}
+
+	// 左のレーンに移動を開始
+	public void MoveToLeft()
+	{
+		if (controller.isGrounded && targetLane > MinLane) {
+			targetLane--;
+		}
+	}
+
+	// 右のレーンに移動を開始
+	public void MoveToRight()
+	{
+		if (controller.isGrounded && targetLane < MaxLane) {
+			targetLane++;
+		}
+	}
+
+	public void Jump()
+	{
+		if (controller.isGrounded) {
+			moveDirection.y = speedJump;
+
+			// ジャンプトリガーを設定
+			animator.SetTrigger("jump");
+		}
+	}
+
+}
