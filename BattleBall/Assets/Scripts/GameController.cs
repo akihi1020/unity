82cdec7519ec9060bb3dd694500cf81e6ba6e7e0
Akihisa Sakai <akihi1020@gmail.com>
2017-06-21 21:37:03 +0900

    gameClear演出追加

--- BattleBall/Assets/Scripts/GameController.cs
+++ BattleBall/Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -9,6 +10,15 @@ public class GameController : MonoBehaviour
     public BallController ballController;
     public Hole hole;
     public LifePanel lifePanel;
+    public GameObject guide;
+    public GameObject gameClear;
+    public GameObject gameOver;
+
+    void Start()
+    {
+        gameClear.SetActive(false);
+        gameOver.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,14 +28,16 @@ public class GameController : MonoBehaviour
 
         // クリアして遷移
         if (hole.IsFallIn() == true) {
-            Debug.Log("Clear");
+            guide.SetActive(false);
+            gameClear.SetActive(true);
             // これ以降のUpdateは止める
             enabled = false;
 
             // 2秒後にReturnToTitleを呼び出す
             Invoke("ReturnToTitle", 2.0f);
         } else if (life.getLifeCount() <= 0 && ballController.IsSleeping()) {
-            Debug.Log("GemeOver");
+            guide.SetActive(false);
+            gameOver.SetActive(true);
             // これ以降のUpdateは止める
             enabled = false;
 
